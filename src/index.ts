6c20848cac57b5962ce6a9da295fae28f594e127
Loreley <mail@lory.dev>
2022-06-18 22:14:16 +0200

    preversion

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {DMXInterface, getConnectedInterfaces} from "usbdmx-js";
+import {dmxnet} from "dmxnet";
+
+const processArgs = process.argv.slice(2);
+
+const allInterfaces = getConnectedInterfaces();
+const availableInterface = allInterfaces[0];
+if (availableInterface === undefined) {
+    console.error("No Interface detected! Exiting...")
+    process.exit(1);
+}
+
+console.info("Found Interface!")
+
+const usbInterface = DMXInterface.open(availableInterface.path);
+var mode = 6;
+const modeFlag = processArgs.find((e) => e.includes("mode="))
+if (modeFlag !== undefined) {
+    const _modeFromFlag = modeFlag.split("=")[1];
+    if (!isNaN(parseInt(_modeFromFlag))) {
+        mode = parseInt(_modeFromFlag)
+    }
+}
+
+console.info(`Using mode ${mode}`)
+usbInterface.setMode(mode);
+
+const dmxnetManager = new dmxnet();
+const artnetReceiver = dmxnetManager.newReceiver();
+console.info("Initialized Art-net interface")
+artnetReceiver.on("data", (data) => {
+    console.log(data);
+})
